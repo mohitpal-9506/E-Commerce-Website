64cd82088eb3ca9b5a823d6dcbb58cb13def97f6
mohitpal-9506 <mohitpal9506@gmail.com>
2026-06-25 13:38:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
 import mysql.connector
-app = Flask(
-    __name__,
-    template_folder="../templates",
-    static_folder="../static"
-)
 
+app = Flask(__name__)
 @app.route('/')
 def home():
 
